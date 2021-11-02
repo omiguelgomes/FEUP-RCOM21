9ef5b55d900eccbc65030d4870412b9e36bca101
Miguel Gomes <47975307+omiguelgomes@users.noreply.github.com>
2021-11-02 09:43:41 +0000

    Added class1 files

---
+++ noncanonical.c
@@ -0,0 +1,100 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255], str[255];
+
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  
+    
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    int i = 0;
+    while (STOP == FALSE) {       /* loop for input */
+      if(read(fd,buf,1) == -1) {
+        STOP = TRUE;
+        break;
+      }
+      if( i == 0) printf("1: %s\n", buf[0]);
+      str[i] = buf[0];
+      if (buf[0]=='\0' || i == 254) STOP=TRUE;
+      i++;
+    }
+    printf("String: %s\n", str);
+
+    //fflush(NULL);
+
+    res = write(fd, str, i);
+    if (res == -1){
+      printf("Error\n");
+    }
+    else printf("Bytes sent: %i\n", res);
+  /* 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
+  */
+
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return 0;
+}
